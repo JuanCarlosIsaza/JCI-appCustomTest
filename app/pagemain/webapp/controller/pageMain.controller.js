797ee5112340b9f9efcc10b4c0e5ae0654bee603
JuanCarlosIsaza <juancarlos.isaza@stratesys-ts.com>
2024-09-19 11:52:59 +0000

    IconTabFilter

--- app/pagemain/webapp/controller/pageMain.controller.js
+++ app/pagemain/webapp/controller/pageMain.controller.js
@@ -1,12 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/ComponentContainer"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller, ComponentContainer) {
+        "use strict";
 
-    return Controller.extend("pagemain.pagemain.controller.pageMain", {
-        onInit: function () {
+        return Controller.extend("pagemain.pagemain.controller.pageMain", {
+            onInit: function () {
 
-        }
+            },
+
+            onFilterSelect: function (oEvent) {
+                debugger;
+                var sKey = oEvent.getParameter("key");
+                var oView = this.getView();
+                var oContainer = oView.byId("contentContainer");
+
+                // Limpia el contenedor antes de agregar la nueva vista
+                oContainer.removeAllItems();
+
+                // Carga la aplicación correspondiente según el ítem seleccionado
+                if (sKey === "Ofr") {
+                    var oComponentContainer = new ComponentContainer({
+                        name: "maintainpassengers",
+                        height: "100%",
+                        width: "100%"
+                    });
+                    oContainer.addItem(oComponentContainer);
+                } else if (sKey === "Sol") {
+                    // Agrega lógica para cargar la vista de "Solicitudes"
+                } else if (sKey === "Seg") {
+                    // Agrega lógica para cargar la vista de "Seguimientos"
+                }
+            }
+        });
     });
-});
