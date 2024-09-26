abbc5a578ae0706415dcf87794df4b1391d3c3a5
JuanCarlosIsaza <juancarlos.isaza@stratesys-ts.com>
2024-09-26 18:24:56 +0000

    sap.ui.loader.config 2

--- app/pagemain/webapp/Component.js
+++ app/pagemain/webapp/Component.js
@@ -6,43 +6,39 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/Device",
     "pagemain/pagemain/model/models"
-],
-    function (UIComponent, Device, models) {
-        "use strict";
+], function (UIComponent, Device, models) {
+    "use strict";
 
-        return UIComponent.extend("pagemain.pagemain.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend("pagemain.pagemain.Component", {
+        metadata: {
+            manifest: "json"
+        },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // Configuración del loader
-                sap.ui.loader.config({
-                    paths: {
-                        "onlymainjs": "./",
-                        "sap/ui/core": "https://sapui5.hana.ondemand.com/1.128.1/resources/sap/ui/core",
-                        "sap/ui/generic/app": "https://sapui5.hana.ondemand.com/1.128.1/resources/sap/ui/generic/app"
-                    }
-                });
+        init: function () {
+            // Configuración del loader
+            sap.ui.loader.config({
+                paths: {
+                    "onlymainjs": "./",
+                    "sap/ui/core": "https://sapui5.hana.ondemand.com/1.128.1/resources/sap/ui/core",
+                    "sap/ui/generic/app": "https://sapui5.hana.ondemand.com/1.128.1/resources/sap/ui/generic/app"
+                }
+            });
 
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+            // Llamar a la función init del padre
+            UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+            // Habilitar la navegación
+            this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
+            // Configurar el modelo de dispositivo
+            this.setModel(models.createDeviceModel(), "device");
 
-                // // register child applications
-                // jQuery.sap.registerModulePath("maintainpassengers", "../maintainpassengers");
-                // jQuery.sap.registerModulePath("onlymainjs", "../onlymainjs");
-            }
-        });
-    }
-);
+            // Configurar el modelo de internacionalización
+            var i18nModel = new sap.ui.model.resource.ResourceModel({
+                bundleName: "pagemain.pagemain.i18n.i18n"
+            });
+            this.setModel(i18nModel, "i18n");
+        }
+    });
+});
+
